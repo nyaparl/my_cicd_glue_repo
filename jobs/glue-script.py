abe4ca44e53b6b3230bb0067739379cc82da722f
nyaparl <161057479+nyaparl@users.noreply.github.com>
2024-06-14 11:36:39 +0530

    Update glue-script.py

--- jobs/glue-script.py
+++ jobs/glue-script.py
@@ -3,8 +3,8 @@ import sessionCreation
 
 spark = createSession()
 
-input_path = "s3://my-bucket-2024-05-31-new/input/emp.txt"
-output_path = "s3://my-bucket-2024-05-31-new/output/emp.json"
+input_path = "s3://mycicdglue-bucket/input/emp.txt"
+output_path = "s3://mycicdglue-bucket/output/emp.json"
 
 df = spark.read.format("csv").load(input_path)
 

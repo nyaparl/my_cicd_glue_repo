3e97c20091d4d399c4c498b71bf4829a765b56fc
nyaparl <161057479+nyaparl@users.noreply.github.com>
2024-06-15 17:48:14 +0530

    Update glue-script.py

--- jobs/glue-script.py
+++ jobs/glue-script.py
@@ -10,3 +10,9 @@ output_path = "s3://mybuck-yap01/output/emp.json"
 df = spark.read.format("csv").load(input_path)
 
 df.coalesce(1).write.mode("overwrite").format("json").save(output_path)
+
+output_path = "s3://mybuck-yap01/output/dept"
+
+df1 = df.groupBy(col("DEPARTMENT_ID")).count().alias("no_of_emps")
+
+df1.coalesce(1).write.mode("overwrite").format("csv").save(output_path)

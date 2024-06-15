6f2cc961c51304454b17fc0595d4851ce91ec2f4
nyaparl <161057479+nyaparl@users.noreply.github.com>
2024-06-15 17:53:21 +0530

    Update glue-script.py

--- jobs/glue-script.py
+++ jobs/glue-script.py
@@ -7,7 +7,7 @@ spark =  SparkSession.builder.appName("cicd").master("local[*]").getOrCreate()
 input_path = "s3://mybuck-yap01/input/emp.csv"
 output_path = "s3://mybuck-yap01/output/emp.json"
 
-df = spark.read.format("csv").load(input_path)
+df = spark.read.format("csv").option('header','true').load(input_path)
 
 df.coalesce(1).write.mode("overwrite").format("json").save(output_path)
 

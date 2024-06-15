426ea5fe49bbb6dbc25c68d95359864632be7ee5
nyaparl <161057479+nyaparl@users.noreply.github.com>
2024-06-15 22:26:27 +0530

    Delete modules directory

--- modules/sessionCreation.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-
-def createSession():
-  return SparkSession.builder.appName('cicd') \
-         .master('local[*]').getOrCreate()
